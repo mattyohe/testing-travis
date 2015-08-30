9f59fe6810132f7f8aeb6ec5c1d5a4d9eb10718c
Matt Yohe <matt.yohe@gmail.com>
2015-08-30 17:01:42 -0400

    Initial

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Matthew Yohe on 8/30/15.
+//  Copyright © 2015 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
